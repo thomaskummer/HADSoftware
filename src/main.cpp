54ff7e69850d309a80d2e1848057577da264148b
Thomas KUMMER <kummetho@gmail.com>
2018-11-27 21:48:10 +0100

    cll

--- src/main.cpp
+++ src/main.cpp
@@ -46,7 +46,7 @@ int main(int argc, char** argv)
     // InteractiveMode iMode;
     // iMode.run();
     
-    if ( cmd("-m") )
+    if ( cmdLineParser("-m") )
     {
         HeartrateController hrc;
         hrc.setup();
