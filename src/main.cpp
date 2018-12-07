2f459323c4696ee8c8f1c3266a1b4788b840bc9b
thomaskummer <kummetho@gmail.com>
2018-12-07 12:08:50 +0100

    tests

--- src/main.cpp
+++ src/main.cpp
@@ -39,11 +39,11 @@ int main(int argc, char** argv)
     HeartrateController hrc;
     hrc.readCmdLineArguments(argc, argv);
         
-    if (hrc.cmdLineParser().feature("-gui"))
-    {
+//    if (hrc.cmdLineParser().feature("-gui"))
+//    {
         hrc.runControllerFromGUI();
-    }
-    else hrc.runConrollerFromCmdLine();
+//    }
+//    else hrc.runConrollerFromCmdLine();
     
     //  maxonMotor.SetObject(0x6062,0x00,0,4);
     //  maxonMotor.GetObject(0x6064,0x00,4);
