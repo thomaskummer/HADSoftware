e99668307cc5982435d28bd51969dea0408ec4c1
thomaskummer <kummetho@gmail.com>
2019-01-05 18:42:56 +0100

    gui files integrated

---
+++ build/CMakeFiles/HeartbeatController.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HeartbeatController.dir/src/main.cpp.o"
+  "HeartbeatController.pdb"
+  "HeartbeatController"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HeartbeatController.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
